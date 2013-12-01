79b2b563aeccb1531b1cb67b0781c772b2ba07de
Kensuke Mitsuzawa <kensuke-mi@is.naist.jp>
2013-12-01 13:06:33 +0900

    a little bit commit

--- construct_bigdoc_or_classifier.py
+++ construct_bigdoc_or_classifier.py
@@ -4,12 +4,17 @@
 ラベルからいわゆるbig documentを作成する．階層別に分けれる様にするのが理想
 """
 __date__='2013/11/30'
+libsvm_wrapper_path='/home/kensuke-mi/opt/libsvm-3.17/python/';
 
 import pickle, argparse, re, codecs, os, glob, json, sys;
+sys.path.append(libsvm_wrapper_path);
+from liblinearutil import *;
+from svmutil import *;
 import return_range;
 import numpy;
 from nltk.corpus import stopwords;
 from nltk import word_tokenize; 
+from sklearn import svm;
 from sklearn.metrics import classification_report;
 from sklearn.cross_validation import train_test_split;
 from sklearn.svm import LinearSVC;
@@ -17,7 +22,8 @@ from sklearn.metrics import confusion_matrix;
 from scipy.sparse import lil_matrix
 stopwords = stopwords.words('english');
 symbols = ["'", '"', '`', '.', ',', '-', '!', '?', ':', ';', '(', ')'];
-
+#option parameter
+put_weight_constraint=True;
 def make_filelist(dir_path):
     file_list=[];
     for root, dirs, files in os.walk(dir_path):
@@ -93,7 +99,7 @@ def construct_2nd_level(parent_node, sub_thompson_tree):
 
 def construct_class_training_1st(parent_node, all_thompson_tree):
     """
-    一層目を指定した時に，一層目の各ラベルに属する単語から文書を作って返す
+    一層目を指定した時に，一層目の各ラベルに属する単語から訓練事例を作って返す
     """
     class_training_stack=[];
     for child_tree_key in all_thompson_tree[parent_node]:
@@ -127,62 +133,173 @@ def cleanup_bigdocument_stack(filename, big_document_stack, stop):
     
     return tokens_s;
 
-def cleanup_class_stack(class_training_stack):
+def preprocess_particular_case(sentence):
+    if re.search(ur'\w\.\w', sentence):
+        sentence=re.sub(ur'(\w)\.(\w)', ur'\1 \2', sentence);
+        return sentence; 
+    else:
+        return sentence;
+
+def cleanup_class_stack(class_training_stack, stop):
     tokens_set_stack=[];
     for tuple_item in class_training_stack:
         label=tuple_item[0];
-        tokens=word_tokenize(tuple_item[1]);
+        cleaned_sentence=preprocess_particular_case(tuple_item[1]);
+        tokens=word_tokenize(cleaned_sentence);
         tokens_s=[t.lower() for t in tokens]
-        tokens_set_stack.append(tokens_s) 
+        if stop==True:
+            tokens_set_stack.append([t for t in tokens if t not in stopwords and t not in symbols]);
+        else:
+            tokens_set_stack.append(tokens_s) 
     return tokens_set_stack;
 
-def make_feature_set(feature_max, feature_map, tokens_set_stack):
+def make_feature_set(feature_map, label_name, tokens_set_stack, feature_mode, stop):
     """
     素性関数を作り出す（要はただのmap）
     """
-    for token_instance in tokens_set_stack:
-        for token in token_instance:
-            if token not in feature_map:
-                feature_map[token]=feature_max;
-                feature_max+=1;
-    return feature_max, feature_map;
+    if feature_mode=='hard':
+        for token_instance in tokens_set_stack:
+            for token in token_instance:
+                hard_cluster_feature=u'hard_{}_{}_unigram'.format(label_name, token);
+                character_feature=hard_cluster_feature;
+                if stop==True and token not in stopwords and token not in symbols:
+                    if token in feature_map and character_feature not in feature_map[token]:
+                        feature_map[token].append(character_feature);
+                    else:
+                        feature_map[token]=[character_feature];
+                elif stop==False:
+                    if token in feature_map and character_feature not in feature_map[token]:
+                        feature_map[token].append(character_feature);
+                    else:
+                        feature_map[token]=[character_feature];
+    #allはすでにsoftの素性が存在しているときに付与
+    elif feature_mode=='all':
+        for token_instance in tokens_set_stack:
+            for token in token_instance:
+                normal_cluster_feature=u'all_{}_unigram'.format(token);
+                character_feature=normal_cluster_feature; 
+                if stop==True and token not in stopwords and token not in symbols:
+                    if token in feature_map and character_feature not in feature_map[token]:
+                        feature_map[token].append(character_feature);
+                    else:
+                        feature_map[token]=[character_feature];
+                elif stop==False:
+                    if token in feature_map and character_feature not in feature_map[token]:
+                        feature_map[token].append(character_feature);
+                    else:
+                        feature_map[token]=[character_feature];
+    return feature_map;
+def make_soft_char_feature(training_map, feature_map, stop):
+    """
+    Dutch folktale corpusの中でマルチラベルのtokenを見つけ出す．
+    見つかったマルチラベルなtokenはword_union_mapに保存して，その後，soft_から始まる素性を作成して，feature_map_characterに登録する．
+    """ 
+    word_cap_map={}; 
+    for label_1st in training_map:
+        token_1st=[]; token_2nd=[];
+        for token in training_map[label_1st]: [token_1st.append(t) for t in token];
+        tokens_set_1st=set(token_1st);
+        for label_2nd in training_map:
+            if label_1st!=label_2nd:
+                for token in training_map[label_2nd]: [token_2nd.append(t) for t in token];
+                tokens_set_2nd=set(token_2nd);
+                cap_2_set=tokens_set_1st.intersection(tokens_set_2nd);
+                for cap_token in cap_2_set:
+                    if cap_token not in word_cap_map:
+                        word_cap_map[cap_token]=[label_1st, label_2nd];
+                    else:
+                        if label_2nd not in word_cap_map[cap_token]:
+                            word_cap_map[cap_token].append(label_2nd);
+    #ちょっと無駄な処理になるが，もう一度for文を回して，capでない素性をつくりだす
+    #ある特定の文書にしか登場していない素性
+    for label in training_map:
+        for tokens in training_map[label]:
+            for token in tokens:
+                if token not in word_cap_map:
+                    if stop==True and token not in stopwords and token not in symbols:
+                        feature_map[token]=[u'soft_{}_{}_unigram'.format(label, token)];
+                    elif stop==False:
+                        feature_map[token]=[u'soft_{}_{}_unigram'.format(label, token)];
+    for key in word_cap_map:
+        L=word_cap_map[key];
+        L.sort();
+        soft_feature=u'soft_{}_{}_unigram'.format(u'_'.join(L), key);
+        if stop==True and token not in stopwords and token not in symbols:
+            if key in feature_map:
+                feature_map[key].append(soft_feature);
+            else:
+                feature_map[key]=[soft_feature];
+        elif stop==False:
+            if key in feature_map:
+                feature_map[key].append(soft_feature);
+            else:
+                feature_map[key]=[soft_feature];
+    return feature_map;
 
-def construct_classifier_for_1st_layer(all_thompson_tree, stop, dutch):
+def make_numerical_feature(feature_map_character):
+    feature_map_numeric={};
+    feature_num_max=1;
+    for token_key in feature_map_character:
+        for feature in feature_map_character[token_key]:
+            feature_map_numeric[feature]=feature_num_max; 
+            feature_num_max+=1;
+    return feature_map_numeric;
+
+def construct_classifier_for_1st_layer(all_thompson_tree, stop, dutch, thompson):
     training_map={};
-    feature_map={};
-    feature_max=0;
+    feature_map_character={};
     num_of_training_instance=0;
-    if dutch==True: 
+    if dutch==True:
+        #TODO ここ，きちんと文書単位に変更する．bigdocumentの状態はよくない
         dir_path='../dutch_folktale_corpus/given_script/translated_big_document/second_layer/' 
         for filepath in make_filelist(dir_path):
             alphabet_label=(os.path.basename(filepath))[0];
             tokens_in_label=word_tokenize(codecs.open(filepath, 'r', 'utf-8').read());
-            for token in tokens_in_label:
-                if token not in feature_map:
-                    feature_map[token]=feature_max;
-                    feature_max+=1;
-            num_of_training_instance+=1;
+            if stop==True:
+                tokens_in_label=[t for t in tokens_in_label if t not in stopwords and t not in symbols];
+            #事前にall素性として追加しておく
+            feature_map_character=make_feature_set(feature_map_character, None, [tokens_in_label], 'all', stop);
             if alphabet_label in training_map:
                 training_map[alphabet_label].append(tokens_in_label);
             else:
                 training_map[alphabet_label]=[tokens_in_label];
+        #A~Zのラベル間でunionな単語を求めだす
+        #全ラベル間でunionな単語を作成して，{token}:'unionなラベルをアンダースコア接続で表現'
+        feature_map_character=make_soft_char_feature(training_map, feature_map_character, stop);
+        for alphabet_label in training_map:
+            print u'The num. of training instance for {} in dutch corpus is {}'.format(alphabet_label, len(training_map[alphabet_label]));
+        print u'-'*30;
+    if thompson==True: 
+        for key_1st in all_thompson_tree:
+            parent_node=key_1st;
+            class_training_stack=construct_class_training_1st(parent_node, all_thompson_tree);
+            tokens_set_stack=cleanup_class_stack(class_training_stack, stop);
+            print u'-'*30;
+            print u'Training instances for {} from thompson tree:{}'.format(key_1st,len(tokens_set_stack));
+            num_of_training_instance+=len(tokens_set_stack);
+            #文字情報の素性関数を作成する
+            feature_map_character=make_feature_set(feature_map_character, key_1st, tokens_set_stack, 'hard', stop);
+            feature_map_character=make_feature_set(feature_map_character, key_1st, tokens_set_stack, 'all', stop);
+            if key_1st in training_map:
+                training_map[key_1st]+=tokens_set_stack;
+            else:
+                training_map[key_1st]=tokens_set_stack;
     
-    for key_1st in all_thompson_tree:
-        parent_node=key_1st;
-        class_training_stack=construct_class_training_1st(parent_node, all_thompson_tree);
-        tokens_set_stack=cleanup_class_stack(class_training_stack);
-        num_of_training_instance+=len(tokens_set_stack);
-        feature_max, feature_map=make_feature_set(feature_max, feature_map, tokens_set_stack);
-        if key_1st in training_map:
-            training_map[key_1st]+=tokens_set_stack;
-        else:
-            training_map[key_1st]=tokens_set_stack;
-    with codecs.open('classifier/feature_map_1st.json', 'w', 'utf-8') as f:
-        json.dump(feature_map, f, indent=4, ensure_ascii=False);
-    feature_space=len(feature_map);
+    with codecs.open('classifier/feature_map_character_1st.json', 'w', 'utf-8') as f:
+        json.dump(feature_map_character, f, indent=4, ensure_ascii=False);
+    #ここで文字情報の素性関数を数字情報の素性関数に変換する
+    feature_map_numeric=make_numerical_feature(feature_map_character);
+    with codecs.open('classifier/feature_map_numeric_1st.json', 'w', 'utf-8') as f:
+        json.dump(feature_map_numeric, f, indent=4, ensure_ascii=False);
+
+    feature_space=len(feature_map_numeric);
+    print u'The number of feature is {}'.format(feature_space)
     #自分で作成したトレーニングモデルがちょっと信用できないので，libsvmも使ってみる
-    out_to_libsvm_format(training_map, feature_map);
-    
+    out_to_libsvm_format(training_map, feature_map_numeric, feature_map_character);
+   
+    """
+    num_of_correct_training_instance=0;
+    num_of_incorrect_training_instance=0;
     #ここからtraining用のコードを書き始める
     #training setを構築して，trainingまでをはしらせる
     for label_index, label_name in enumerate(training_map):
@@ -191,6 +308,7 @@ def construct_classifier_for_1st_layer(all_thompson_tree, stop, dutch):
         training_data_label=[];
         instances_in_correct_label=training_map[label_name];
         for col_number, one_instance in enumerate(instances_in_correct_label):
+            num_of_correct_training_instance+=1;
             for feature_token in one_instance:
                 #stopwordsの除去するか or not
                 if stop==True:
@@ -208,6 +326,7 @@ def construct_classifier_for_1st_layer(all_thompson_tree, stop, dutch):
             if not incorrect_label_name==label_name:
                 instances_in_incorrect_label=training_map[incorrect_label_name];
                 for col_number, one_instance in enumerate(instances_in_incorrect_label):
+                    num_of_incorrect_training_instance+=1;
                     for feature_token in one_instance:
                         if stop==True:     
                             if feature_token not in stopwords and feature_token not in symbols:
@@ -217,18 +336,21 @@ def construct_classifier_for_1st_layer(all_thompson_tree, stop, dutch):
                         training_matrix[feature_number, col_number]=1;    
                 map(lambda label: training_data_label.append(0), instances_in_incorrect_label);
 
-        print feature_space, num_of_training_instance, len(training_data_label) 
+        print 'Ration of correct label:incorrect label={}:{}'.format(num_of_correct_training_instance,
+                                                                     num_of_incorrect_training_instance);
         tmp=training_matrix.T;
         #この場所でtrainingを実行可能な状態になっているはず
         data_train, data_test, label_train, label_test = train_test_split(tmp,\
                                                                           training_data_label);                
         #分類器にパラメータを与える
-        estimator = LinearSVC(C=1.0)
-        #与える型はnumpy.ndarrayでないといけない
-        #トレーニングデータで学習する
-        estimator.fit(tmp, training_data_label)
-
-        #テストデータの予測をする
+        if put_weight_constraint==True:
+            print 'put weight!'
+            estimator = LinearSVC(C=1.0, class_weight='auto');
+            estimator.fit(data_train, label_train);
+        else:
+            estimator = LinearSVC(C=1.0)
+            estimator.fit(data_train, label_train);
+        #テストデータでチェック
         label_predict = estimator.predict(data_test)
 
         print '-'*30;
@@ -241,29 +363,51 @@ def construct_classifier_for_1st_layer(all_thompson_tree, stop, dutch):
         filename='{}_classifier.pickle'.format(label_name);
         with codecs.open('./classifier/1st_layer/'+filename, 'w', 'utf-8') as f:
             pickle.dump(estimator, f);
-
-def out_to_libsvm_format(training_map, feature_map):
+            """
+def out_to_libsvm_format(training_map, feature_map_numeric, feature_map_character):
+    ratio_map={'C':0, 'N':0};
     for correct_label_key in training_map:
-        outfile=codecs.open('./classifier/libsvm_format/'+correct_label_key, 'w', 'utf-8');
+        outfile=codecs.open('./classifier/libsvm_format/'+correct_label_key+'.data', 'w', 'utf-8');
         instances_in_correct_label=training_map[correct_label_key];
         for one_instance in instances_in_correct_label:
+            ratio_map['C']+=1;
             one_instance_stack=[];
-            for token in one_instance: 
-                feature_number=feature_map[token];
-                one_instance_stack.append(str(feature_number)+u':1');
-            outfile.write(u'{} {}\n'.format('1', u' '.join(one_instance_stack)));
+            for token in one_instance:
+                #liblinearのフォーマットに対応させるために(0始まりは認められないので)
+                for character_feature in feature_map_character[token]:
+                    feature_number=feature_map_numeric[character_feature];  
+                    one_instance_stack.append(feature_number);
+            one_instance_stack=list(set(one_instance_stack));
+            one_instance_stack.sort();
+            one_instance_stack=[str(number)+u':1' for number in one_instance_stack];
+            outfile.write(u'{} {}\n'.format('+1', u' '.join(one_instance_stack)));
         for incorrect_label_key in training_map:
             if not correct_label_key==incorrect_label_key:
                 instances_in_incorrect_label=training_map[incorrect_label_key];
                 for one_instance in instances_in_incorrect_label:
+                    ratio_map['N']+=1;
                     one_instance_stack=[];
                     for token in one_instance:
-                        feature_number=feature_map[token];
-                        one_instance_stack.append(str(feature_number)+u':1');
+                        for character_feature in feature_map_character[token]:
+                            feature_number=feature_map_numeric[character_feature];  
+                            one_instance_stack.append(feature_number);
+                    one_instance_stack=list(set(one_instance_stack))
+                    one_instance_stack.sort();
+                    one_instance_stack=[str(number)+u':1' for number in one_instance_stack];
                     outfile.write(u'{} {}\n'.format('-1', u' '.join(one_instance_stack)));
         outfile.close();
+        train_y, train_x=svm_read_problem('./classifier/libsvm_format/'+correct_label_key+'.data'); 
+        ratio_c=float(ratio_map['C']) / (ratio_map['C']+ratio_map['N']);
+        ratio_n=float(ratio_map['N']) / (ratio_map['C']+ratio_map['N']);
+        weight_parm='-w-1 {} -w1 {} -s 2 -q'.format(int(ratio_c*100), int(ratio_n*100));
+        weight_parm_svm='-w-1 {} -w1 {}'.format(int(ratio_c*100), int(ratio_n*100));
+        print weight_parm
+        model=train(train_y, train_x, weight_parm);
+        #svm_model=svm_train(train_y, train_x, weight_parm_svm);
+        save_model('./classifier/liblinear/'+correct_label_key+'.liblin.model', model);
+        #svm_save_model('./classifier/libsvm/'+correct_label_key+'.svm.model', svm_model);
 
-def main(level, mode, all_thompson_tree, stop, dutch):
+def main(level, mode, all_thompson_tree, stop, dutch, thompson):
     level=int(level);
     level_big_document={};
     #result_stack=return_range.find_sub_tree(input_motif_no, all_thompson_tree) 
@@ -293,10 +437,10 @@ def main(level, mode, all_thompson_tree, stop, dutch):
     elif mode=='class':
         training_map={};
         feature_map={};
-        feature_max=0;
+        feature_max=1;
         num_of_training_instance=0;
         if level==1:
-            construct_classifier_for_1st_layer(all_thompson_tree, stop, dutch)
+            construct_classifier_for_1st_layer(all_thompson_tree, stop, dutch, thompson)
 
 if __name__=='__main__':
     parser=argparse.ArgumentParser(description='');
@@ -304,7 +448,8 @@ if __name__=='__main__':
     parser.add_argument('-mode', '--mode', help='classification problem(class) or big-document(big)', required=True);
     parser.add_argument('-stop', help='If added, stop words are eliminated from training file', action='store_true');
     parser.add_argument('-dutch', help='If added, document from dutch folktale database is added to training corpus', action='store_true');
+    parser.add_argument('-thompson', help='If added, outline from thompson tree is added to training corpus', action='store_true');
     args=parser.parse_args();
     dir_path='./parsed_json/'
     all_thompson_tree=return_range.load_all_thompson_tree(dir_path);
-    result_stack=main(args.level, args.mode, all_thompson_tree, args.stop, args.dutch);
+    result_stack=main(args.level, args.mode, all_thompson_tree, args.stop, args.dutch, args.thompson);
